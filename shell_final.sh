446e96809c92973de9637412f8b685186ab55f97
kseymin <kseymin@gmail.com>
2018-03-12 04:51:57 +0900

    plz

--- shell_final.sh
+++ shell_final.sh
@@ -1,19 +1,22 @@
-#!/bin/bash
-
-sudo apt-get update
+#!/bin/bash -e
+cd ~
+udo apt-get update
 sudo apt-get install git-core curl zlib1g-dev build-essential libssl-dev libreadline-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev libcurl4-openssl-dev python-software-properties libffi-dev
 
 cd
 git clone https://github.com/rbenv/rbenv.git ~/.rbenv
-echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bashrc
-echo 'eval "$(rbenv init -)"' >> ~/.bashrc
+echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.profile
+echo 'eval "$(rbenv init -)"' >> ~/.profile
+source ~/.profile
 
 git clone https://github.com/rbenv/ruby-build.git ~/.rbenv/plugins/ruby-build
-echo 'export PATH="$HOME/.rbenv/plugins/ruby-build/bin:$PATH"' >> ~/.bashrc
+echo 'export PATH="$HOME/.rbenv/plugins/ruby-build/bin:$PATH"' >> ~/.bash_profile
+source ~/.profile
+
 
 git clone https://github.com/rbenv/rbenv-gem-rehash.git ~/.rbenv/plugins/rbenv-gem-rehash
 
-. ~/.bashrc
+source  ~/.profile
 
 rbenv install 2.4.0
 rbenv global 2.4.0
@@ -24,4 +27,4 @@ gem install rails -v 5.0.6
 rbenv rehash
 echo "Finished installing rails"
 rails -v
-rbenv rehash
+rbenv rehash                                      
