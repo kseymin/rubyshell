cfb3f273074002389088b8096b1d882d72dd59f8
kseymin <kseymin@gmail.com>
2018-03-12 01:55:52 +0900

    shell change

--- shell1.sh
+++ shell1.sh
@@ -9,6 +9,7 @@ sudo apt-get update
 sudo apt-get install git-core curl zlib1g-dev build-essential libssl-dev libreadline-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev libcurl4-openssl-dev python-software-properties libffi-dev nodejs
 
 # Install rbenv and ruby-build plugins
+cd
 git clone https://github.com/rbenv/rbenv.git ~/.rbenv
 echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bashrc
 echo 'eval "$(rbenv init -)"' >> ~/.bashrc
