36ba6f51472acf622bb5fc5a8fb67cd1d981f795
kseymin <kseymin@gmail.com>
2018-03-12 01:49:34 +0900

    shell change

--- shell1.sh
+++ shell1.sh
@@ -5,7 +5,7 @@ set -e
 cd ~
 
 # Install some dependencies for Ruby
-sudo apt-get updat
+sudo apt-get update
 sudo apt-get install git-core curl zlib1g-dev build-essential libssl-dev libreadline-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev libcurl4-openssl-dev python-software-properties libffi-dev nodejs
 
 # Install rbenv and ruby-build plugins
