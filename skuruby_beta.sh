f6668b0117bec87349da9fd15f817a93b11c3e21
kseymin <kseymin@gmail.com>
2018-03-17 01:11:50 +0900

    test 코드 추가

---
+++ skuruby_beta.sh
@@ -0,0 +1,34 @@
+#!/bin/bash -e
+echo "insert ruby version:"
+read rv
+echo "insert rails version:"
+read railsv
+
+
+cd ~
+
+sudo apt-get update
+sudo apt-get install git-core curl zlib1g-dev build-essential libssl-dev libreadline-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev libcurl4-openssl-dev python-software-properties libffi-dev
+
+
+git clone https://github.com/rbenv/rbenv.git ~/.rbenv
+echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bashrc
+echo 'eval "$(rbenv init -)"' >> ~/.bashrc
+
+
+git clone https://github.com/rbenv/ruby-build.git ~/.rbenv/plugins/ruby-build
+echo 'export PATH="$HOME/.rbenv/plugins/ruby-build/bin:$PATH"' >> ~/.bashrc
+
+
+git clone https://github.com/rbenv/rbenv-gem-rehash.git ~/.rbenv/plugins/rbenv-gem-rehash
+
+rbenv install $rv
+rbenv global $rv
+ruby -v
+
+gem install bundler
+gem install rails -v $railsv
+rbenv rehash
+echo "Finished installing rails"
+rails -v
+rbenv rehash
